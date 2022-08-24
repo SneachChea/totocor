01afc9e44a137e0e4e85e95646d512d0400807dc
Shaheen Acheche <shaheenacheche@MacBook-Pro-de-Shaheen.local>
2022-08-24 16:01:47 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+
+setup(name='totocor',
+      version='1.0',
+      description='Easy AutoCorrelation program for signal processing',
+      author='SneachChea',
+      packages=[],
+     )
